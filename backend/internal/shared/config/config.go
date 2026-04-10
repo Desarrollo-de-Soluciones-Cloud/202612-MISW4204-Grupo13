ed9be8f46f69607e9229c4e1336a15c181476a82
juandirodrib12 <141871531+juandirodrib12@users.noreply.github.com>
2026-04-09 23:37:01 -0500

    fix: arreglo variables de entorno y conexion

--- backend/internal/shared/config/config.go
+++ backend/internal/shared/config/config.go
@@ -14,11 +14,11 @@ type Config struct {
 func Load() *Config {
 	return &Config{
 		Port:       getEnv("PORT", "8080"),
-		DBHost:     getEnv("DB_HOST", "localhost"),
-		DBPort:     getEnv("DB_PORT", "5432"),
-		DBUser:     getEnv("DB_USER", "postgres"),
-		DBPassword: getEnv("DB_PASSWORD", "postgres"),
-		DBName:     getEnv("DB_NAME", "postgres"),
+		DBHost:     getEnv("DB_HOST", ""),
+		DBPort:     getEnv("DB_PORT", ""),
+		DBUser:     getEnv("DB_USER", ""),
+		DBPassword: getEnv("DB_PASSWORD", ""),
+		DBName:     getEnv("DB_NAME", ""),
 	}
 }
 
